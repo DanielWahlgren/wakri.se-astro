0f85b1aa15ae00efcab3e203d7f8fc352233a467
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2026-05-01 12:03:55 +0000

    publish

--- astro-sample/astro.config.mjs
+++ astro-sample/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://danielwahlgren.github.io',
+  base: '/wakri.se-astro',
+});

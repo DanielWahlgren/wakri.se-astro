718e88193e3a8198153f81c2e726e22ae682a207
Daniel Wahlgren <daniel.wahlgren@gmail.com>
2026-05-01 12:15:19 +0000

    CNAME

--- astro-sample/astro.config.mjs
+++ astro-sample/astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://danielwahlgren.github.io',
-  base: '/wakri.se-astro',
+  site: 'https://astro.wakri.se',
 });
